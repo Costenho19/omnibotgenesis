39cece733e01c245c20ed991f0abe6a1b1e7998c
Costenho19 <madridpep3@gmail.com>
2025-07-18 14:05:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -688,9 +688,9 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
   
   # Main principal
 if __name__ == "__main__":
-application = Application.builder().token(TELEGRAM_TOKEN).build()
-application.add_handler(CommandHandler("start", start))
-application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
-    application.run_polling()
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
+        application.run_polling()
 
 
