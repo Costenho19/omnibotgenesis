597d17b259f75dbcb2844729a86c60e060ab2369
Costenho19 <madridpep3@gmail.com>
2025-07-27 11:23:43 -0700

    Update pqc_encryption.py

--- pqc_encryption.py
+++ pqc_encryption.py
@@ -1,21 +1,14 @@
-from pqcrypto.sign.dilithium2 import generate_keypair, sign, verify
-from pqcrypto.exceptions import SignatureError
+from cryptography.fernet import Fernet
+import base64
+import os
 
-class PQCEncryption:
-    def __init__(self, private_key=None, public_key=None):
-        if private_key and public_key:
-            self.public_key = public_key
-            self.private_key = private_key
-        else:
-            self.public_key, self.private_key = generate_keypair()
+# Simulamos clave post-cuántica con Fernet por ahora
+key = base64.urlsafe_b64encode(os.urandom(32))
+cipher = Fernet(key)
 
-    def sign_message(self, message: str) -> bytes:
-        return sign(message.encode(), self.private_key)
+def encrypt_message(message: str) -> str:
+    return cipher.encrypt(message.encode()).decode()
 
-    def verify_signature(self, message: str, signature: bytes) -> bool:
-        try:
-            verify(message.encode(), signature, self.public_key)
-            return True
-        except SignatureError:
-            return False
+def decrypt_message(token: str) -> str:
+    return cipher.decrypt(token.encode()).decode()
 
