f3da796b61731601b32d7cced99271e50058233b
Costenho19 <madridpep3@gmail.com>
2025-07-29 23:54:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -240,3 +240,11 @@ async def main():
         webhook_url=webhook_url
     )
 
+if __name__ == "__main__":
+    import asyncio
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(main())
